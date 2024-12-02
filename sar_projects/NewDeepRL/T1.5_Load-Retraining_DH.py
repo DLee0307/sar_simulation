733efb0fea0639c731ad081d4fa3313187f2c466
DLee0307 <thakd037@gmail.com>
2024-12-01 20:58:50 -0500

    2500 trainstep

--- sar_projects/NewDeepRL/T1.5_Load-Retraining_DH.py
+++ sar_projects/NewDeepRL/T1.5_Load-Retraining_DH.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     # 저장된 모델 및 리플레이 버퍼 로드
     RL_Manager.load_model(
-        t_step_load=500, 
+        t_step_load=2000, 
         GroupName='', 
         LogName='SOV5_A30_L200_0deg_aRot90_S3D', 
         Params_only=False, 
